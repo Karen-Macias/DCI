a63b645bea4ed8563f6b0b5f72d9baa16c703d55
Alma Gonzalez <alxogm@Almas-MacBook-Air-2.local>
2018-06-26 13:58:48 -0500

    Dia 1 de trabajo verano.

---
+++ lic_verano/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.linspace(-5,5,20)
+y=x**2
+print y,x
+plt.plot(x,y,'.-r')
+
+plt.savefig('plot.png')
+plt.show()
+plt.clf()
+plt.scatter(x,np.exp(x))
+plt.show()
+
+#a=5
+#print 'La raiz cuadrada de 5 es:', np.sqrt(a)
